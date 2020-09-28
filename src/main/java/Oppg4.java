a79e7d69e0439d28988791ecb34dc00a0d327e5f
Joakim Skogø Langvand <jlangvand@gmail.com>
2020-09-28 18:36:54 +0200

    Implement assignemnt 2. Restructuring.

--- src/main/java/Oppg4.java
+++ src/main/java/Oppg4.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import java.util.Random;
 import java.util.Scanner;
@@ -11,10 +13,13 @@ import java.util.Scanner;
 public class Oppg4 {
     public static void main(String... args) {
 
-        //assignment1();
+        SharedUtils.printDebug = false;
+
+        assignment1();
 
         Assignment2 assignment2 = new Assignment2();
-        assignment2.setup();
+        assignment2.setup((int)1e7, (int)1e9);
+        assignment2.run();
 
     }
 
@@ -28,7 +33,7 @@ public class Oppg4 {
         Person[] people = readPeopleFromFile(System.getProperty("user.dir") + "/names");
         for (Person person : people) table.add(person);
 
-        // Tests: Joakim should be present in the table, Svlad is made up
+        // Tests: Joakim should be present in the table, Svlad is made up and should fail
         Optional<Person> joakim = table.search("Langvand, Joakim Skogø");
         Optional<Person> svlad = table.search("Cjelli, Svlad");
         System.out.println("\nSearch tests " + (joakim.isPresent() && !svlad.isPresent() ? "OK" : "FAILED"));
@@ -105,22 +110,69 @@ class Assignment2 {
     private int[] intArray;
     private MyHashTable<Integer> hashtable;
     private boolean ready;
+    private SimpleTimer timer;
 
     Assignment2() {
+        timer = new SimpleTimer();
         this.ready = false;
         SharedUtils.printHeader("Assignment 2:\nPopulate hashtable with 10M ints and measure perf.");
     }
 
-    public boolean setup() {
-        return true;
+    /*
+      Initialise fields. Returns false if something went wrong.
+    */
+    public void setup(int size, int bound) {
+        this.timer.start();
+        this.intArray = getRandomIntArray(size, bound);
+        this.hashtable = new MyHashTable((int)((double)size * 1.35));
+        SharedUtils.print("Setup complete in " + this.timer.stop() + "s");
+        this.ready = true;
+    }
+
+    /*
+      Default parameters
+    */
+    public void setup() {
+        this.setup((int)10e8, (int)10e7);
     }
 
     public boolean run() {
         if (!ready) {
-            System.out.println("Run setup first");
+            SharedUtils.print("Run setup first");
             return false;
         }
-        return true;
+
+        boolean status = true;
+
+        this.timer.start();
+        for (int i = 0; i < intArray.length; hashtable.add(intArray[i++]));
+        double duration = this.timer.stop();
+
+        java.util.HashMap hashmap = new java.util.HashMap();
+
+        this.timer.start();
+        for (int i = 0; i < intArray.length; hashmap.put(null, intArray[i++]));
+        double duration2 = this.timer.stop();
+
+        SharedUtils.print("Length of array: " + intArray.length, true);
+
+        SharedUtils.print("Custom hashtable: " + duration + "s");
+        SharedUtils.print("Java hashtable: " + duration2 + "s");
+
+        int collisions = hashtable.getCollisions();
+        double load = ((double)((int)(hashtable.getLoad() * 100)/100.0));
+        int overhead = (int)((1 - load) * 100);
+
+        status = overhead > 35 ? false : status;
+
+        //SharedUtils.print(hashtable + "\n", true);
+        SharedUtils.print("Collisions: " + hashtable.getCollisions() +
+                          " (~" + + (int)(((double)collisions / (double)intArray.length)*100) +
+                          "%), Load: ~" + load +
+                          " (~" + overhead +
+                          "% overhead) Rehashes: " + hashtable.getRehashes(), false);
+
+        return status;
     }
 
     /*
@@ -134,7 +186,24 @@ class Assignment2 {
     }
 }
 
+class SimpleTimer {
+    private Instant instant;
+
+    SimpleTimer() {
+        this.instant = Instant.now();
+    }
+
+    public void start() {
+        this.instant = Instant.now();
+    }
+
+    public double stop() {
+        return (double)ChronoUnit.MICROS.between(instant, Instant.now()) * 1.0e-6;
+    }
+}
+
 class SharedUtils {
+    public static boolean printDebug = false;
     public static void printHeader(String str) {
         int cols = 80;
         String lines[] = str.split("\n");
@@ -142,10 +211,8 @@ class SharedUtils {
         for (String line : lines) {
             int len = line.length();
             System.out.print("\n*");
-            //for (int i = 1; i < (cols / 2) - (len / 2); i++) System.out.print(" ");
             printNTimes(" ", (cols / 2) - (len / 2));
             System.out.print(line);
-            //for (int i = (cols / 2) + (len / 2) + (len % 2); i < cols - 1; i++) System.out.print(" ");
             printNTimes(" ", (cols / 2) - (len / 2) - (len % 2) - 2);
             System.out.print("*");
         }
@@ -156,6 +223,14 @@ class SharedUtils {
 
     static void printNTimes(String str, int n) {
         for (int i = 0; i < n; i++) System.out.print(str);
+    }
+
+    static void print(String str, boolean debug) {
+        if (!debug || printDebug) print(str);
+    }
+
+    static void print(String str) {
+        System.out.println(str);
     }
 }
 
@@ -177,21 +252,48 @@ class MyHashTable<T> {
     private int size;
     private int entries;
     private int collisions;
-    private float loadFactor;
+    private double loadFactor;
     private LinkedList<T>[] table;
+    private int rehashes;
+
+    public static int defaultSize = 51;
+    public static double defaultLoadFactor = 0.75;
 
     MyHashTable() {
-        this.size = 13;
+        this(defaultSize, defaultLoadFactor);
+    }
+
+    MyHashTable(int size) {
+        this(size, defaultLoadFactor);
+    }
+
+    MyHashTable(int size, double loadFactor) {
+        this.size = nextPrime(size);
         this.entries = 0;
-        this.loadFactor = 0.95f;
+        this.loadFactor = loadFactor;
+        this.rehashes = 0;
         this.table = (LinkedList<T>[])java.lang.reflect.Array.newInstance(LinkedList.class, this.size);
         for (int i = 0; i < this.size; i++) table[i] = new LinkedList<T>();
     }
 
     /*
-      Search for an object in the table. Returns the object if present.
+      Get number of collisions
      */
-    public Optional<T> search(Object cmp) {
+    public int getCollisions() {
+        int collisions = 0, size = 0;
+        for (int i = 0; i < this.size; size = this.table[i++].getSize())
+            collisions += size > 1 ? --size : 0;
+        return collisions;
+    }
+
+    public int getRehashes() {
+        return this.rehashes;
+    }
+
+    /*
+      Search for an object in the table. Returns the object if present.
+    */
+    public Optional<T> search(String cmp) {
         int index = genHash(cmp);
         Optional<T> ret = Optional.empty();
         for (int i = 0; i < this.table[index].getSize(); i++) {
@@ -203,9 +305,9 @@ class MyHashTable<T> {
 
     /*
       Get the current load of the table
-     */
-    public float getLoad() {
-        return (float)this.entries / (float)this.size;
+    */
+    public double getLoad() {
+        return (double)this.entries / (double)this.size;
     }
 
     /*
@@ -233,7 +335,7 @@ class MyHashTable<T> {
       We'll use toString() to hash the object.
 
       Before returning, we'll check if the load exceeds the load factor and rehash if necessary.
-     */
+    */
     public void add(T obj) {
         int index = genHash(obj);
         table[index].push(obj);
@@ -243,7 +345,7 @@ class MyHashTable<T> {
 
     /*
       Pretty print.
-     */
+    */
     public String toString() {
         String out = "Contents of hashtable. Asterisk indicates collision.\n Index    Count     Value(s)";
         int count;
@@ -257,7 +359,7 @@ class MyHashTable<T> {
 
     /*
       Rehash the table. This one could be nicer.
-     */
+    */
     private void rehash() {
         int oldSize = this.size;
         // Increase the table by ~35%
@@ -271,22 +373,35 @@ class MyHashTable<T> {
                 newTable[genHash(obj)].push(obj);
             }
         this.table = newTable;
+        this.rehashes++;
     }
 
-    /*
-      Characters are weighted by position. This could probably be improved.
-     */
     private int genHash(Object obj) {
         String str = obj.toString();
         int hash = 0;
-        for (int i = 0; i < str.length(); hash += (int)str.charAt(i) * ++i);
-        return hash % this.size;
+
+        // If the object is a Number, treat it as an int.
+        // This would not perform good on floats..
+        if (obj instanceof java.lang.Number) {
+            // Hashing method by @Thomas Mueller
+            // https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+            hash = Integer.parseInt(str);
+            hash = ((hash >>> 16) ^ hash) * 0x45d9f3b;
+            hash = ((hash >>> 16) ^ hash) * 0x45d9f3b;
+            hash = (hash >>> 16) ^ hash;
+        } else {
+            // This should do for strings.
+            // Chars are weighted by position, so 'foo' and 'oof' produce unique hashes.
+            for (int i = 0; i < str.length(); hash += (int)str.charAt(i++) + i);
+        }
+
+        return (hash > 0 ? hash : - hash) % this.size;
     }
 }
 
 /*
   How to implement a linked list in five minutes. Let's come back to this to improve it..
- */
+*/
 class LinkedList<T> {
     private Node<T> first;
 
